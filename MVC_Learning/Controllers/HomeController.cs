e1cd84360ed4b267fa81596d4a47f8dd3aa0cfab
Marek <marconayt@gmail.com>
2019-04-06 03:31:40 +0200

    1st commit

---
+++ MVC_Learning/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Learning.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 17 ? "Dzień dobry" : "Dobry wieczór";
+            return View();
+        }
+    }
+}
