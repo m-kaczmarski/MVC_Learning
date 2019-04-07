d541d18444f38ae6827d48492aafb61276fa0d61
Marek <marconayt@gmail.com>
2019-04-07 05:09:54 +0200

    commit

--- MVC_Learning/Controllers/HomeController.cs
+++ MVC_Learning/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVC_Learning.Models;
 
 namespace MVC_Learning.Controllers
 {
@@ -14,6 +15,19 @@ namespace MVC_Learning.Controllers
             int hour = DateTime.Now.Hour;
             ViewBag.Greeting = hour < 17 ? "Dzień dobry" : "Dobry wieczór";
             return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            // do zrobienia: wyślij zawartość guestResponse do organizatora przyjęcia            
+            return View("Thanks", guestResponse); 
         }
     }
 }
